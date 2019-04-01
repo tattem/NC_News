299faa59fd2d2387b0304e31d1626e7ae259d0b2
Northcoders10 <northcoders10@Northcoders10s-MacBook-Pro.local>
2019-04-01 14:16:23 +0100

    edits timestamp in migration tables

--- db/migrations/20190401120558_articles_table.js
+++ db/migrations/20190401120558_articles_table.js
@@ -7,7 +7,7 @@ exports.up = function(knex, Promise) {
       articlesTable.integer('votes').defaultTo(0)
       articlesTable.text('topic').references('topics.slug')
       articlesTable.text('author').references('users.username')
-      articlesTable.timestamp('created_at').defaultTo(knex.fn.now(6))
+      articlesTable.datetime('created_at').defaultTo(knex.fn.now(6))
   });
 };
 
