83fc6f34e2cb848f3f71f3d577f78f7988daefa5
Northcoders10 <northcoders10@Northcoders10s-MacBook-Pro.local>
2019-04-02 11:52:06 +0100

    tests the utils function convertArticleData

--- db/seeds/seed.js
+++ db/seeds/seed.js
@@ -18,7 +18,7 @@ exports.seed = (knex, Promise) => {
     .then(() => {
       // run a method that takes the article data and converts it.
       const convertedArticles = convertArticleData(articleData);
-      // console.log(convertedArticles, '<<< converted articles')
+      console.log(convertedArticles, '<<< converted articles')
       return knex('articles')
         .insert(convertedArticles)
         .returning('*');
