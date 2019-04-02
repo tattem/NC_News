e0941e4ab3eb00f9060a580c6e28505fb372b94c
Northcoders10 <northcoders10@Northcoders10s-MacBook-Pro.local>
2019-04-02 16:13:34 +0100

    passes testing for utils - convertCommentData

--- spec/utils.spec.js
+++ spec/utils.spec.js
@@ -1,14 +1,48 @@
 const { expect } = require('chai');
 const { convertArticleData } = require('../utils/convertArticleData');
+const { convertCommentData } = require('../utils/convertCommentData');
 
 describe('convertArticleData', () => {
-    it('returns a non mutated array', () => {
-        const articles = []
-        expect(convertArticleData(articles)).to.not.equal(articles)
-    });
-    it('returns a converted timestamp array', () => {
-        const articles = [{created_at: 1542284514171}, {created_at: 1416140514171}, {created_at: 1289996514171}]
-        const convertedArticles = convertArticleData(articles)
-        expect(`${convertedArticles[0].created_at}`).to.equal('Thu Nov 15 2018 12:21:54 GMT+0000 (Greenwich Mean Time)')
-    });
+  it('returns a non mutated array', () => {
+    const articles = [];
+    expect(convertArticleData(articles)).to.not.equal(articles);
+  });
+  it('returns a converted timestamp array', () => {
+    const articles = [
+      { created_at: 1542284514171 },
+      { created_at: 1416140514171 },
+      { created_at: 1289996514171 }
+    ];
+    const convertedArticles = convertArticleData(articles);
+    expect(`${convertedArticles[0].created_at}`).to.equal(
+      'Thu Nov 15 2018 12:21:54 GMT+0000 (Greenwich Mean Time)'
+    );
+  });
+});
+
+describe('convertCommentData', () => {
+  it('returns an array of objects containing certain keys', () => {
+    const comments = [
+      {
+        body:
+          "Oh, I've got compassion running out of my nose, pal! I'm the Sultan of Sentiment!",
+        belongs_to: "They're not exactly dogs, are they?",
+        created_by: 'butter_bridge',
+        votes: 16,
+        created_at: 1511354163389
+      }
+    ];
+    const articles = [
+      {
+        article_id: 1,
+        title: "They're not exactly dogs, are they?",
+        topic: 'mitch',
+        author: 'butter_bridge',
+        body: 'Well? Think about it.',
+        created_at: 533132514171
+      }
+    ];
+    const convertedComments = convertCommentData(comments, articles)
+    expect(convertedComments[0]).to.contain.keys('author', 'article_id' , 'created_at', 'body')
+  });
 });
